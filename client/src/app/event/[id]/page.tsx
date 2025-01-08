5dd9f194e7613d91dfd2eeed1c1225d94d8c3d63
MbusyaNgesa <theengesa@gmail.com>
2025-01-08 23:42:53 +0300

    Setting up shadcn

---
+++ client/src/app/event/[id]/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>AfroSayari</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <div className="p-4 bg-cyan-700">
+        <h1 className="text-2xl">Eventlify</h1>
+      </div>
+    </div>
+  );
+};
